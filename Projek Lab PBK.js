2fcbbe44a60250baa9feef999d8f8b43965f6523
Muhammad Fattah <muhammadfattah619@gmail.com>
2021-06-11 21:22:15 +0700

    style

--- Projek Lab PBK.js
+++ Projek Lab PBK.js
@@ -22,21 +22,21 @@ lib.ssMetadata = [
 
 
 
-(lib.CachedBmp_24 = function() {
+(lib.CachedBmp_15 = function() {
 	this.initialize(ss["Projek Lab PBK_atlas_4"]);
 	this.gotoAndStop(0);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.CachedBmp_23 = function() {
+(lib.CachedBmp_14 = function() {
 	this.initialize(ss["Projek Lab PBK_atlas_4"]);
 	this.gotoAndStop(1);
 }).prototype = p = new cjs.Sprite();
 
 
 
-(lib.CachedBmp_22 = function() {
+(lib.CachedBmp_13 = function() {
 	this.initialize(ss["Projek Lab PBK_atlas_4"]);
 	this.gotoAndStop(2);
 }).prototype = p = new cjs.Sprite();
@@ -64,7 +64,7 @@ lib.ssMetadata = [
 
 
 
-(lib.CachedBmp_21 = function() {
+(lib.CachedBmp_9 = function() {
 	this.initialize(ss["Projek Lab PBK_atlas_3"]);
 	this.gotoAndStop(3);
 }).prototype = p = new cjs.Sprite();
@@ -184,13 +184,13 @@ p._updateVisibility = _updateVisibility;
 	this.initialize(mode,startPosition,loop,{});
 
 	// Layer_1
-	this.instance = new lib.CachedBmp_22();
+	this.instance = new lib.CachedBmp_13();
 	this.instance.setTransform(-166.45,-57.45,0.2804,0.2804);
 
-	this.instance_1 = new lib.CachedBmp_23();
+	this.instance_1 = new lib.CachedBmp_14();
 	this.instance_1.setTransform(-166.45,-57.45,0.2804,0.2804);
 
-	this.instance_2 = new lib.CachedBmp_24();
+	this.instance_2 = new lib.CachedBmp_15();
 	this.instance_2.setTransform(-166.45,-57.45,0.2804,0.2804);
 
 	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance}]}).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_2}]},1).wait(1));
@@ -270,7 +270,7 @@ p._updateVisibility = _updateVisibility;
 	this.initialize(mode,startPosition,loop,{});
 
 	// Layer_1
-	this.instance = new lib.CachedBmp_21();
+	this.instance = new lib.CachedBmp_9();
 	this.instance.setTransform(-304.4,-107.45,0.4574,0.4574);
 
 	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
@@ -595,17 +595,17 @@ lib.properties = {
 	color: "#FFFFFF",
 	opacity: 1.00,
 	manifest: [
-		{src:"images/CachedBmp_1.png?1623420892529", id:"CachedBmp_1"},
-		{src:"images/Projek Lab PBK_atlas_.png?1623420892462", id:"Projek Lab PBK_atlas_"},
-		{src:"images/Projek Lab PBK_atlas_2.png?1623420892462", id:"Projek Lab PBK_atlas_2"},
-		{src:"images/Projek Lab PBK_atlas_3.png?1623420892463", id:"Projek Lab PBK_atlas_3"},
-		{src:"images/Projek Lab PBK_atlas_4.png?1623420892463", id:"Projek Lab PBK_atlas_4"},
-		{src:"images/Projek Lab PBK_atlas_5.png?1623420892464", id:"Projek Lab PBK_atlas_5"},
-		{src:"https://code.jquery.com/jquery-3.4.1.min.js?1623420892529", id:"lib/jquery-3.4.1.min.js"},
-		{src:"components/sdk/anwidget.js?1623420892529", id:"sdk/anwidget.js"},
-		{src:"components/ui/src/textinput.js?1623420892529", id:"an.TextInput"},
-		{src:"components/ui/src/css.js?1623420892529", id:"an.CSS"},
-		{src:"components/ui/src/button.js?1623420892529", id:"an.Button"}
+		{src:"images/CachedBmp_1.png?1623421269369", id:"CachedBmp_1"},
+		{src:"images/Projek Lab PBK_atlas_.png?1623421269314", id:"Projek Lab PBK_atlas_"},
+		{src:"images/Projek Lab PBK_atlas_2.png?1623421269314", id:"Projek Lab PBK_atlas_2"},
+		{src:"images/Projek Lab PBK_atlas_3.png?1623421269314", id:"Projek Lab PBK_atlas_3"},
+		{src:"images/Projek Lab PBK_atlas_4.png?1623421269315", id:"Projek Lab PBK_atlas_4"},
+		{src:"images/Projek Lab PBK_atlas_5.png?1623421269315", id:"Projek Lab PBK_atlas_5"},
+		{src:"https://code.jquery.com/jquery-3.4.1.min.js?1623421269369", id:"lib/jquery-3.4.1.min.js"},
+		{src:"components/sdk/anwidget.js?1623421269369", id:"sdk/anwidget.js"},
+		{src:"components/ui/src/textinput.js?1623421269369", id:"an.TextInput"},
+		{src:"components/ui/src/css.js?1623421269369", id:"an.CSS"},
+		{src:"components/ui/src/button.js?1623421269370", id:"an.Button"}
 	],
 	preloads: []
 };
